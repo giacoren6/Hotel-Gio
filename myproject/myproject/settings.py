e9f2bd70966b84b1bf201029d1b0c476d3f6bd54
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-05-23 18:46:16 +0000

    preparing for heroku

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -146,3 +146,6 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url 
+DATABASES['default'] = dj_database_url.config()
