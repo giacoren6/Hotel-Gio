787b3b0259c3aa24ef97bae04503471737b1c8ea
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-05-23 17:53:19 +0000

    changing file for heroku

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-z5v05i9!+=vgmao868uu4(-jg!c7u2k867t%s*1*@@g&$wf-s!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myapp-heroku-app.herokuapp.com, localhost,']
+ALLOWED_HOSTS = ['*']
 
 
 
