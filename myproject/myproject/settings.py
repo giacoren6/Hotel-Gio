3f9ce8e74564ef0ef053dbbbb7c6a663b0dfd100
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-05-23 18:26:24 +0000

    set up for heroku

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z5v05i9!+=vgmao868uu4(-jg!c7u2k867t%s*1*@@g&$wf-s!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['myproject-heroku-1.herokuapp.com']
 
 
 
