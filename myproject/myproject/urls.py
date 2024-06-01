5b334711ea5c7e9d119e7954a0dc0a533dcd35c1
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-06-01 15:27:41 +0100

    Update urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,20 +34,13 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('book_done/', views.book_done, name='book_done'),
     path('login_done/', views.login_done, name='login_done'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
+
 
 
 ]
-
-
-
-from django.conf import settings
-from django.conf.urls import url
-from django.views.static import serve
-
-
-url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-
 
 
 
