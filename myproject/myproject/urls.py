462e4e2164ba932c45cc5276297b38e07066a664
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-06-01 15:22:33 +0100

    Update urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -36,3 +36,14 @@ urlpatterns = [
 
 
 
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+
+
+url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
+
+
+
